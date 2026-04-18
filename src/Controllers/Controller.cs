13405a6a3942c4ec0d3b4f2d0ed03989f6515c10
1301263Vanessalmeida <1301263@estudante.uab.pt>
2026-04-18 15:41:00 +0100

    Update Controller.cs

--- src/Controllers/Controller.cs
+++ src/Controllers/Controller.cs
@@ -1,1 +1,26 @@
+using System.Collections.Generic;
 
+public class Controller
+{
+    private View view;
+    private Model model;
+
+    public Controller(View view, Model model)
+    {
+        this.view = view;
+        this.model = model;
+
+        view.OnFilesSelected += HandleFilesSelected;
+        model.OnMergeCompleted += HandleMergeCompleted;
+    }
+
+    private void HandleFilesSelected(List<string> files)
+    {
+        model.ValidateFiles(files);
+    }
+
+    private void HandleMergeCompleted()
+    {
+        view.ShowResult();
+    }
+}
