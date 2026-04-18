5695684679660ff8325dc4b1bb00870937b73a39
1301263Vanessalmeida <1301263@estudante.uab.pt>
2026-04-18 15:39:36 +0100

    Update Model.cs

--- src/Models/Model.cs
+++ src/Models/Model.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
 
+public class Model
+{
+    public event Action OnMergeCompleted;
+
+    public void ValidateFiles(List<string> files)
+    {
+        Console.WriteLine("Validar ficheiros...");
+        MergeFiles(files);
+    }
+
+    public void MergeFiles(List<string> files)
+    {
+        Console.WriteLine("Juntar ficheiros PDF...");
+        OnMergeCompleted?.Invoke();
+    }
+}
