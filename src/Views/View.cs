9e280a9213dc3f87b93b09682e3dbe850a753c49
1301263Vanessalmeida <1301263@estudante.uab.pt>
2026-04-18 15:39:01 +0100

    Update View.cs

--- src/Views/View.cs
+++ src/Views/View.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
 
+public class View
+{
+    public event Action<List<string>> OnFilesSelected;
+
+    public void SelectFiles()
+    {
+        Console.WriteLine("Selecionar ficheiros...");
+        OnFilesSelected?.Invoke(new List<string> { "file1.pdf", "file2.pdf" });
+    }
+
+    public void ShowResult()
+    {
+        Console.WriteLine("PDF pronto para download.");
+    }
+}
