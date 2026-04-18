011c91846cf16e24b2792423995cda0de867a869
1301263Vanessalmeida <1301263@estudante.uab.pt>
2026-04-18 15:41:33 +0100

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,11 @@
+class Program
+{
+    static void Main()
+    {
+        View view = new View();
+        Model model = new Model();
+        Controller controller = new Controller(view, model);
 
+        view.SelectFiles();
+    }
+}
